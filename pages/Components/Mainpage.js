3afb3f1022bfd9f5f51b1461c869853c5d8a1888
copninich <48505@yupparaj.ac.th>
2021-11-16 22:37:48 +0700

    change text

--- pages/Components/Mainpage.js
+++ pages/Components/Mainpage.js
@@ -8,9 +8,9 @@ export default class Mainpage extends Component {
                     <div className="page-header min-vh-100" style={{background: "linear-gradient(111.94deg,#ffeb3b 0%,#e91e63 65%,#673ab7 100%);"}}>
                     <div className="container">
                         <div className="row">
-                        <div className="col-lg-7 text-center mx-auto">
-                            <h1 className="text-white pt-3 mt-n5 animate__animated  animate__bounceInUp"><i className="fad fa-construction"></i> Under Construction!</h1>
-                            <p className="lead text-white mt-3 animate__animated animate__bounceInDown">1 September 2021 New <i className="fad fa-browser"></i> www.kampanart.studio<br /> Kampanart Chaimooltan #Dek65 Computer Engineering</p>
+                        <div className="col-lg-8 text-center mx-auto">
+                            <h1 className="text-white pt-3 mt-n5 animate__animated  animate__bounceInUp"><i className="fad fa-construction"></i> เว็บไซต์ไม่สามารถให้บริการได้!</h1>
+                            <p className="lead text-white mt-3 animate__animated animate__bounceInDown">เนื่องจากการดับกระแสไฟฟ้า เพื่อซ่อมบำรุง ของทางการไฟฟ้าส่วนภูมิภาค เชียงใหม่<br/>ทำให้ Server ที่บริการฐานข้อมูลดับไปด้วย <br/>ในวันพุธ ที่ 17 พฤศจิกายน 2564 เวลา 09.00 - 16.00<br/>ขออภัยในความไม่สะดวก แก่คณะกรรมการทุกท่าน ที่มาเยี่ยมชม</p>
                             <a href="https://old.kampanart.studio" className="btn bg-gradient-primary btn-round mb-0 me-1 mt-2 mt-md-0 w-75 animate__animated animate__bounceInDown" style={{fontSize:'17px'}} >old website</a>
                         </div>
                         </div>
